a52bfed2883ec29a34d10d90f8c285e10ace5970
Aasif Ali Khan <a.a.khan@se16.qmul.ac.uk>
2019-08-05 23:52:04 +0100

    gui weather app
    gui weather app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Authors: Sehbaan, Yaseen, Jaymin, Aasif, Maksym
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
